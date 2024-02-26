ae2c2d4099aabc0e86e657334bd2c51b3374a088
Laura Pinckney <laura.pinckney@yahoo.com>
2024-02-26 14:54:56 +0000

    card component

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const Card = () => {
   return (
-    <div>
-      <img alt="robots" src="https://robohash.org/test" />
+    <div className="bg-light-green">
+      <img alt="robots" src="https://robohash.org/test?200x200" />
       <div>
         <h2>Jane Doe</h2>
         <p>jane.doe@gmail.com</p>
--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import { Card } from './Card';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card />
+    <div>
+      <Card />
+      <Card />
+      <Card />
+    </div>
   </React.StrictMode>
 );
