ea734522faf1921a4489376b9894cdc58c36fb75
Laura Pinckney <laura.pinckney@yahoo.com>
2024-02-27 11:54:43 +0000

    css updated for Search and App components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { CardList } from './CardList';
 import { robots } from './robots';
+import { SearchBox } from './SearchBox';
 
 export const App = () => {
-  return <CardList robots={robots} />;
+  return (
+    <div className="tc">
+      <h1>RoboFriends</h1>
+      <SearchBox />
+      <CardList robots={robots} />
+    </div>
+  );
 };
