82a5fdeb43a2e1c2a942a353496e1afeef440e97
Laura Pinckney <laura.pinckney@yahoo.com>
2024-02-27 10:37:52 +0000

    update to cardComponent via refactoring

--- src/CardList.jsx
+++ src/CardList.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { Card } from './Card';
 
 export const CardList = ({ robots }) => {
-  const cardComponent = robots.map((user, i) => {
-    return (
-      <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
-    );
-  });
-  return <div>{cardComponent}</div>;
+  return (
+    <div>
+      {robots.map((user, i) => {
+        return (
+          <Card
+            key={robots[i].id}
+            id={robots[i].id}
+            name={robots[i].name}
+            email={robots[i].email}
+          />
+        );
+      })}
+    </div>
+  );
 };
